7259841a24503681fa586156a136602fa86b73b1
Sebastian Bugge <vkbugge@hotmail.com>
2023-04-19 14:02:03 +0200

    feat: add user database model

---
+++ api/src/main/java/no/freshify/api/model/User.java
@@ -0,0 +1,23 @@
+package no.freshify.api.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
