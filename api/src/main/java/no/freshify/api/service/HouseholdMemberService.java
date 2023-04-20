e2c170d155198fc036bd74d289aea9f99c6a15ac
Navid <Navidm@stud.ntnu.no>
2023-04-20 14:35:29 +0200

    refac: householdmemberservice throws exception if householdmember already exists

--- api/src/main/java/no/freshify/api/service/HouseholdMemberService.java
+++ api/src/main/java/no/freshify/api/service/HouseholdMemberService.java
@@ -1,9 +1,12 @@
 package no.freshify.api.service;
 
 import lombok.RequiredArgsConstructor;
+import no.freshify.api.exception.HouseholdMemberAlreadyExistsException;
 import no.freshify.api.model.HouseholdMember;
 import no.freshify.api.model.HouseholdMemberKey;
 import no.freshify.api.repository.HouseholdMemberRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,12 +14,17 @@ import org.springframework.stereotype.Service;
 public class HouseholdMemberService {
     private final HouseholdMemberRepository householdMemberRepository;
 
-    public void addHouseholdMember(HouseholdMember householdMember) {
+    Logger logger = LoggerFactory.getLogger(HouseholdMemberService.class);
+
+    public void addHouseholdMember(HouseholdMember householdMember) throws HouseholdMemberAlreadyExistsException {
+        if (householdMemberExists(householdMember.getId())) {
+            logger.warn("Household member already exists");
+            throw new HouseholdMemberAlreadyExistsException();
+        }
         householdMemberRepository.saveAndFlush(householdMember);
     }
 
-    public boolean householdMemberExists(Long householdId, Long userId) {
-        HouseholdMemberKey householdMemberKey = new HouseholdMemberKey(householdId, userId);
+    public boolean householdMemberExists(HouseholdMemberKey householdMemberKey) {
         return householdMemberRepository.existsById(householdMemberKey);
     }
 }
