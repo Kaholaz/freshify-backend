b4d6ae9ea926ed18061a8fc4ed2aa626688f04eb
Navid <Navidm@stud.ntnu.no>
2023-04-20 13:08:30 +0200

    feat: householdmemberrepository method for adding members and for checking if member exists

--- api/src/main/java/no/freshify/api/repository/HouseholdMemberRepository.java
+++ api/src/main/java/no/freshify/api/repository/HouseholdMemberRepository.java
@@ -2,9 +2,12 @@ package no.freshify.api.repository;
 
 import no.freshify.api.model.Household;
 import no.freshify.api.model.HouseholdMember;
+import no.freshify.api.model.HouseholdMemberKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HouseholdMemberRepository extends JpaRepository<HouseholdMember, Long> {
     public HouseholdMember findHouseholdMembersByUser(Long userId);
     public HouseholdMember findHouseholdMembersByHousehold(Long householdId);
+    public HouseholdMember save(HouseholdMember householdMember);
+    boolean existsById(HouseholdMemberKey householdMemberKey);
 }
