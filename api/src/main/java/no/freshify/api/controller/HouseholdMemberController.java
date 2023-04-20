514a5296884a6ce30a248b61d4ef7951ab3c3eb8
Navid <Navidm@stud.ntnu.no>
2023-04-20 14:35:46 +0200

    refac: changed householdmembercontroller to throw exceptions

--- api/src/main/java/no/freshify/api/controller/HouseholdMemberController.java
+++ api/src/main/java/no/freshify/api/controller/HouseholdMemberController.java
@@ -1,6 +1,9 @@
 package no.freshify.api.controller;
 
 import lombok.RequiredArgsConstructor;
+import no.freshify.api.exception.HouseholdMemberAlreadyExistsException;
+import no.freshify.api.exception.HouseholdNotFoundException;
+import no.freshify.api.exception.UserNotFoundException;
 import no.freshify.api.model.*;
 import no.freshify.api.service.HouseholdMemberService;
 import no.freshify.api.service.HouseholdService;
@@ -30,7 +33,7 @@ public class HouseholdMemberController {
     //TODO Remember to add authentication logic and verify access privileges before processing request
 
     @PostMapping("{id}/add")
-    public ResponseEntity<String> addUser(@PathVariable Long id, @RequestBody Map<String, Long> requestBody) {
+    public ResponseEntity<String> addUser(@PathVariable Long id, @RequestBody Map<String, Long> requestBody) throws UserNotFoundException, HouseholdNotFoundException, HouseholdMemberAlreadyExistsException {
         Long userId = requestBody.get("userId");
 
         logger.info("Adding user with id: " + userId + " to household with id: " + id);
@@ -38,13 +41,13 @@ public class HouseholdMemberController {
         User user = userService.getUserById(userId);
         if (user == null) {
             logger.warn("User not found");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+            throw new UserNotFoundException();
         }
 
         Household household = householdService.findHouseholdByHouseholdId(id);
         if (household == null) {
             logger.warn("Household not found");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Household not found");
+            throw new HouseholdNotFoundException();
         }
 
         HouseholdMember householdMember = new HouseholdMember();
@@ -52,11 +55,6 @@ public class HouseholdMemberController {
         householdMember.setHousehold(household);
         householdMember.setRole(HouseholdMemberRole.USER);
         householdMember.setId(new HouseholdMemberKey(household.getId(), user.getId()));
-
-        if (householdMemberService.householdMemberExists(id, userId)) {
-            logger.warn("Household member already exists");
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Household member already exists");
-        }
 
         householdMemberService.addHouseholdMember(householdMember);
         logger.info("Added household member");
