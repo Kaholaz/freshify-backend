3ab4df55c60faa4bdf451fd300a928ab01a01076
Navid <Navidm@stud.ntnu.no>
2023-04-20 13:07:53 +0200

    feat: householdrepository added method for finding household by id

--- api/src/main/java/no/freshify/api/repository/HouseholdRepository.java
+++ api/src/main/java/no/freshify/api/repository/HouseholdRepository.java
@@ -3,5 +3,6 @@ package no.freshify.api.repository;
 import no.freshify.api.model.Household;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface HouseholdRepository extends JpaRepository<Household, Long> {
+public interface HouseholdRepository extends JpaRepository<Household, Long> {
+    public Household findHouseholdById(Long householdId);
 }
