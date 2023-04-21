a928b287547f2c9dfd21b1c3540138de2a7cfee7
Navid <Navidm@stud.ntnu.no>
2023-04-21 09:49:35 +0200

    feat: added itemservice method for getting inventory items of a household

--- api/src/main/java/no/freshify/api/service/ItemService.java
+++ api/src/main/java/no/freshify/api/service/ItemService.java
@@ -1,15 +1,22 @@
 package no.freshify.api.service;
 
 import lombok.RequiredArgsConstructor;
+import no.freshify.api.model.Household;
 import no.freshify.api.model.Item;
 import no.freshify.api.repository.ItemRepository;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
+
+    public List<Item> getHouseholdItems(Household household) {
+        return itemRepository.findItemsByHousehold(household);
+    }
 
     public void addItem(Item item) {
         itemRepository.save(item);
