7d28c66c85273da7b58c766ac9b9acf81503c80f
Sebastian Bugge <vkbugge@hotmail.com>
2023-04-21 10:11:05 +0200

    fix: comment out secure cookie field to make postman easier to work with

--- api/src/main/java/no/freshify/api/security/CookieFactory.java
+++ api/src/main/java/no/freshify/api/security/CookieFactory.java
@@ -17,7 +17,7 @@ public class CookieFactory {
     public static Cookie getAuthorizationCookie(String token) {
         Cookie cookie = new Cookie("Authorization", token);
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
+        // cookie.setSecure(true);
         cookie.setPath("/");
         cookie.setMaxAge((int) AuthenticationService.TOKEN_DURATION.get(ChronoUnit.SECONDS));
         return cookie;
