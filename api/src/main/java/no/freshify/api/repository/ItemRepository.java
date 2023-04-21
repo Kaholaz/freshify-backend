393595553fd0dcc85495f37a7bd5f396a82be264
Navid <Navidm@stud.ntnu.no>
2023-04-21 10:14:45 +0200

    feat: added new methods to itemrepository

--- api/src/main/java/no/freshify/api/repository/ItemRepository.java
+++ api/src/main/java/no/freshify/api/repository/ItemRepository.java
@@ -6,8 +6,11 @@ import no.freshify.api.model.ItemStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findItemsByHousehold(Household household);
     List<Item> findItemsByHouseholdAndStatus(Long householdId, ItemStatus status);
+    Item getItemById(Long id);
+    Item findByIdAndHousehold(long id, Household household);
 }
--- api/src/main/java/no/freshify/api/service/ItemService.java
+++ api/src/main/java/no/freshify/api/service/ItemService.java
@@ -1,6 +1,7 @@
 package no.freshify.api.service;
 
 import lombok.RequiredArgsConstructor;
+import no.freshify.api.exception.ItemNotFoundException;
 import no.freshify.api.model.Household;
 import no.freshify.api.model.Item;
 import no.freshify.api.repository.ItemRepository;
@@ -16,6 +17,20 @@ public class ItemService {
 
     public List<Item> getHouseholdItems(Household household) {
         return itemRepository.findItemsByHousehold(household);
+    }
+
+    public Item getItemById(long id) throws ItemNotFoundException {
+        Item item = itemRepository.findById(id).orElse(null);
+        if (item == null) {
+            throw new ItemNotFoundException();
+        }
+
+        return item;
+    }
+
+    public void deleteItemById(long id) throws ItemNotFoundException {
+        Item item = getItemById(id);
+        itemRepository.delete(item);
     }
 
     public void addItem(Item item) {
