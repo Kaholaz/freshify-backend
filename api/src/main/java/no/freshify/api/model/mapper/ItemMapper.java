135aa54d0b64a4bd8c068d633dec7c6ba2970c27
Navid <Navidm@stud.ntnu.no>
2023-04-21 16:55:39 +0200

    feat: implemented mapping for mapping both list of and singular ItemType to ItemTypeDTO

--- api/src/main/java/no/freshify/api/model/mapper/ItemMapper.java
+++ api/src/main/java/no/freshify/api/model/mapper/ItemMapper.java
@@ -1,11 +1,17 @@
 package no.freshify.api.model.mapper;
 
 import no.freshify.api.model.Item;
+import no.freshify.api.model.ItemType;
 import no.freshify.api.model.dto.InventoryItem;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(uses = {UserMapper.class})
 public abstract class ItemMapper {
     public abstract InventoryItem toItemDto(Item item);
 
+    public abstract ItemType toItemTypeDTO(ItemType itemType);
+
+    public abstract List<ItemType> toItemTypeDTO(List<ItemType> itemTypes);
 }
