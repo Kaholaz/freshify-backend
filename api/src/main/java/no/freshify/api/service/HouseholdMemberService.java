cefc9933c9f5b25913ccd33f48ec6179a9143100
Navid <Navidm@stud.ntnu.no>
2023-04-21 12:21:34 +0200

    Added method for getting members by member key in householdmemberservice

--- api/src/main/java/no/freshify/api/service/HouseholdMemberService.java
+++ api/src/main/java/no/freshify/api/service/HouseholdMemberService.java
@@ -2,9 +2,11 @@ package no.freshify.api.service;
 
 import lombok.RequiredArgsConstructor;
 import no.freshify.api.exception.HouseholdMemberAlreadyExistsException;
+import no.freshify.api.exception.UserDoesNotBelongToHouseholdException;
 import no.freshify.api.model.HouseholdMember;
 import no.freshify.api.model.HouseholdMemberKey;
 import no.freshify.api.model.HouseholdMemberRole;
+import no.freshify.api.model.User;
 import no.freshify.api.repository.HouseholdMemberRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,5 +29,13 @@ public class HouseholdMemberService {
 
     public boolean householdMemberExists(HouseholdMemberKey householdMemberKey) {
         return householdMemberRepository.existsById(householdMemberKey);
+    }
+
+    public HouseholdMember getHouseholdMemberByHouseholdMemberKey(HouseholdMemberKey householdMemberKey) throws UserDoesNotBelongToHouseholdException {
+        if (this.householdMemberExists(householdMemberKey)) {
+            return householdMemberRepository.findByHouseHoldMemberKey(householdMemberKey);
+        }
+
+        throw new UserDoesNotBelongToHouseholdException();
     }
 }
