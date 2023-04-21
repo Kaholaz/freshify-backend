3c442c6b8618a9a0da31ca0ca4cb37b5b6533811
Navid <Navidm@stud.ntnu.no>
2023-04-21 09:49:19 +0200

    fix: fixed incorrect method in item repository class

--- api/src/main/java/no/freshify/api/repository/ItemRepository.java
+++ api/src/main/java/no/freshify/api/repository/ItemRepository.java
@@ -1,5 +1,6 @@
 package no.freshify.api.repository;
 
+import no.freshify.api.model.Household;
 import no.freshify.api.model.Item;
 import no.freshify.api.model.ItemStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,6 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    List<Item> findItemsByHousehold(Long householdId);
+    List<Item> findItemsByHousehold(Household household);
     List<Item> findItemsByHouseholdAndStatus(Long householdId, ItemStatus status);
 }
