75aa3c1664727b211fc04be85254f811863902ae
Navid <Navidm@stud.ntnu.no>
2023-04-21 11:39:55 +0200

    feat: created IllegalItemParameterException

---
+++ api/src/main/java/no/freshify/api/exception/IllegalItemParameterException.java
@@ -0,0 +1,11 @@
+package no.freshify.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class IllegalItemParameterException extends Exception {
+    public IllegalItemParameterException(String message) {
+        super(message);
+    }
+}
