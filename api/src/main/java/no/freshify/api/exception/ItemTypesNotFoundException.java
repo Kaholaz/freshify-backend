ea23d030ff9b6f6d850e69a190102fcf25802caa
Sebastian Bugge <vkbugge@hotmail.com>
2023-04-26 18:28:39 +0200

    feat: no search results returns empty list

--- api/src/main/java/no/freshify/api/exception/ItemTypesNotFoundException.java
+++ api/src/main/java/no/freshify/api/exception/ItemTypesNotFoundException.java
@@ -3,7 +3,7 @@ package no.freshify.api.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.OK)
 public class ItemTypesNotFoundException extends Exception {
     public ItemTypesNotFoundException() {
         super("No item types matched the search query");
