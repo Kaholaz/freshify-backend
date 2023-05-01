735df85e4f97dfc2a25f0ea6e77cdfe1c501c887
Mats Lading <mjlading@stud.ntnu.no>
2023-05-01 10:33:07 +0200

    feat: add api endpoint for getting lists of sorted waste of a household

--- api/src/main/java/no/freshify/api/repository/ItemRepository.java
+++ api/src/main/java/no/freshify/api/repository/ItemRepository.java
@@ -5,12 +5,14 @@ import no.freshify.api.model.Item;
 import no.freshify.api.model.ItemStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findItemsByHousehold(Household household);
     List<Item> findItemsByHouseholdAndStatus(Household household, ItemStatus status);
     Item getItemById(Long id);
     Item findByIdAndHousehold(long id, Household household);
+    List<Item> findItemsByHouseholdAndStatusAndLastChangedBetweenAndRemainingGreaterThan
+            (Household household, ItemStatus status, Date startDate, Date endDate, Double remaining);
 }
