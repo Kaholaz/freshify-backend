3877da1315fde5d13b56bf1a2ede81c8fa71ba00
Navid <Navidm@stud.ntnu.no>
2023-05-02 18:02:39 +0200

    feat: recipe service created method for filtering recipes by category and by allergens

--- api/src/main/java/no/freshify/api/service/RecipeService.java
+++ api/src/main/java/no/freshify/api/service/RecipeService.java
@@ -1,11 +1,14 @@
 package no.freshify.api.service;
 
 import lombok.RequiredArgsConstructor;
+import no.freshify.api.model.recipe.Allergen;
 import no.freshify.api.model.recipe.Recipe;
 import no.freshify.api.repository.RecipeRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -29,4 +32,14 @@ public class RecipeService {
             throw new RuntimeException("Could not add recipe");
         }
     }
+
+    public Page<Recipe> getRecipesNotContainingAllergensPageable(List<Allergen> allergens, Pageable pageable) {
+        return recipeRepository.findAllNotContainingAllergens(allergens, pageable);
+    }
+
+    public Page<Recipe> getRecipesByCategoryAndNotContainingAllergensPageable(Long categoryId, List<Allergen> allergens, Pageable pageable) {
+        Set<Allergen> allergensSet = new HashSet<>(allergens);
+        return recipeRepository.findByCategoriesIdAndAllergensNotInOrNoAllergens(categoryId, allergensSet, pageable);
+    }
+
 }
