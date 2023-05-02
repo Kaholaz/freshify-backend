f5b15744ee9a03c4367fa4961a20eff3201e4cca
Navid <Navidm@stud.ntnu.no>
2023-05-02 09:48:38 +0200

    change: updated recipe model image logic

--- api/src/main/java/no/freshify/api/model/recipe/Recipe.java
+++ api/src/main/java/no/freshify/api/model/recipe/Recipe.java
@@ -32,6 +32,6 @@ public class Recipe {
     @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
     private Set<RecipeIngredient> recipeIngredients;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    private RecipeImage image;
+    @Column
+    private String image;
 }
