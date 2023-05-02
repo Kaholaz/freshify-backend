31e6b4d20f949228371194c311e78b4b29894bd8
Sebastian Bugge <vkbugge@hotmail.com>
2023-05-02 09:50:49 +0200

    fix: set internal mysql port in docker to 7000 to fix an issue with Mats' docker

--- api/start_database.sh
+++ api/start_database.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 56000:3306 --name freshify_dev_database -e MYSQL_ROOT_PASSWORD=freshify -e MYSQL_DATABASE=freshify_dev -d mysql
+docker run --rm -p 56000:7000 --name freshify_dev_database -e MYSQL_ROOT_PASSWORD=freshify -e MYSQL_DATABASE=freshify_dev -d mysql --port 7000
