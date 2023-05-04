962cc1c26ce318eca4e170ade5c686bf21eda937
Sebastian Bugge <vkbugge@hotmail.com>
2023-05-04 09:20:05 +0200

    fix: add more wait time to upstart

--- api/start_application.sh
+++ api/start_application.sh
@@ -1,6 +1,6 @@
 docker run --rm -p 56000:7000 --name freshify_dev_database -e MYSQL_ROOT_PASSWORD=freshify -e MYSQL_DATABASE=freshify_dev -d mysql --port 7000
 echo "Waiting for database to start..."
-sleep 5
+sleep 10
 mvn spring-boot:start && mvn spring-boot:stop
 docker exec -i freshify_dev_database mysql -uroot -pfreshify < ./src/main/resources/insert_item_types.sql
 mvn spring-boot:run
