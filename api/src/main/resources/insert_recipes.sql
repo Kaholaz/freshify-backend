ad9ad7348c5cff6d5cfb678a9ac46590feba3d0d
Navid <Navidm@stud.ntnu.no>
2023-05-05 10:27:57 +0200

    chore: added more allergens and recipe categories to sql

--- api/src/main/resources/insert_recipes.sql
+++ api/src/main/resources/insert_recipes.sql
@@ -1,9 +1,29 @@
 USE freshify_dev;
-START TRANSACTION;
-INSERT INTO allergen(id,name) VALUES (2,'Lactose');
-INSERT INTO allergen(id,name) VALUES (1,'Gluten');
+INSERT INTO allergen(id, name) VALUES (1, 'Gluten');
+INSERT INTO allergen(id, name) VALUES (2, 'Lactose');
+INSERT INTO allergen(id, name) VALUES (3, 'Shellfish');
+INSERT INTO allergen(id, name) VALUES (4, 'Peanuts');
+INSERT INTO allergen(id, name) VALUES (5, 'Tree Nuts');
+INSERT INTO allergen(id, name) VALUES (6, 'Eggs');
+INSERT INTO allergen(id, name) VALUES (7, 'Fish');
+INSERT INTO allergen(id, name) VALUES (8, 'Soy');
+INSERT INTO allergen(id, name) VALUES (9, 'Wheat');
+INSERT INTO allergen(id, name) VALUES (10, 'Sesame');
+
 
 INSERT INTO recipe_category(id, name) VALUES (1, 'Italian');
 INSERT INTO recipe_category(id, name) VALUES (2, 'Meat Dish');
-COMMIT
+INSERT INTO recipe_category(id, name) VALUES (3, 'Vegetarian');
+INSERT INTO recipe_category(id, name) VALUES (4, 'Vegan');
+INSERT INTO recipe_category(id, name) VALUES (5, 'Dessert');
+INSERT INTO recipe_category(id, name) VALUES (6, 'Appetizer');
+INSERT INTO recipe_category(id, name) VALUES (7, 'Salad');
+INSERT INTO recipe_category(id, name) VALUES (8, 'Soup');
+INSERT INTO recipe_category(id, name) VALUES (9, 'Asian');
+INSERT INTO recipe_category(id, name) VALUES (10, 'Mexican');
+INSERT INTO recipe_category(id, name) VALUES (11, 'Mediterranean');
+INSERT INTO recipe_category(id, name) VALUES (12, 'Beverage');
+INSERT INTO recipe_category(id, name) VALUES (13, 'Breakfast');
+INSERT INTO recipe_category(id, name) VALUES (14, 'Sandwich');
+INSERT INTO recipe_category(id, name) VALUES (15, 'Seafood');
 
