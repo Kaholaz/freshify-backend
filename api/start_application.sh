d29dda0ad2f3097ff8f68ec519290da5a348a573
Sebastian Bugge <vkbugge@hotmail.com>
2023-05-05 22:33:43 +0200

    fix: fix encoding issues introduced by docker

--- api/start_application.sh
+++ api/start_application.sh
@@ -2,10 +2,10 @@ docker run --rm -p 56000:7000 --name freshify_dev_database -e MYSQL_ROOT_PASSWOR
 echo "Waiting for database to start..."
 sleep 30
 mvn spring-boot:start && mvn spring-boot:stop
-docker exec -i freshify_dev_database mysql -uroot -pfreshify < ./src/main/resources/insert_item_types.sql
-docker exec -i freshify_dev_database mysql -uroot -pfreshify < ./src/main/resources/insert_recipes.sql
-docker exec -i freshify_dev_database mysql -uroot -pfreshify < ./src/main/resources/insert_users.sql
-docker exec -i freshify_dev_database mysql -uroot -pfreshify < ./src/main/resources/insert_households.sql
-docker exec -i freshify_dev_database mysql -uroot -pfreshify < ./src/main/resources/insert_shopping_lists.sql
-docker exec -i freshify_dev_database mysql -uroot -pfreshify < ./src/main/resources/insert_items.sql
+docker exec -i freshify_dev_database mysql -uroot -pfreshify --default-character-set=utf8 < ./src/main/resources/insert_item_types.sql
+docker exec -i freshify_dev_database mysql -uroot -pfreshify --default-character-set=utf8 < ./src/main/resources/insert_recipes.sql
+docker exec -i freshify_dev_database mysql -uroot -pfreshify --default-character-set=utf8 < ./src/main/resources/insert_users.sql
+docker exec -i freshify_dev_database mysql -uroot -pfreshify --default-character-set=utf8 < ./src/main/resources/insert_households.sql
+docker exec -i freshify_dev_database mysql -uroot -pfreshify --default-character-set=utf8 < ./src/main/resources/insert_shopping_lists.sql
+docker exec -i freshify_dev_database mysql -uroot -pfreshify --default-character-set=utf8 < ./src/main/resources/insert_items.sql
 mvn spring-boot:run
