d8f2e3e4ec1bc1763253e9090912a85953bc7ebf
Navid <Navidm@stud.ntnu.no>
2023-05-05 09:52:16 +0200

    feat: updated insert_recipes sql to be a transaction

--- api/src/main/resources/insert_recipes.sql
+++ api/src/main/resources/insert_recipes.sql
@@ -1,7 +1,9 @@
 USE freshify_dev;
+START TRANSACTION;
 INSERT INTO allergen(id,name) VALUES (2,'Lactose');
 INSERT INTO allergen(id,name) VALUES (1,'Gluten');
 
 INSERT INTO recipe_category(id, name) VALUES (1, 'Italian');
 INSERT INTO recipe_category(id, name) VALUES (2, 'Meat Dish');
+COMMIT
 
