263a377bef70147868556964bed5f2b1630b0db4
Sebastian Bugge <vkbugge@hotmail.com>
2023-05-05 17:41:22 +0200

    fix: return 401 if the user uses an expired token

--- api/src/main/java/no/freshify/api/security/JwtTokenFilter.java
+++ api/src/main/java/no/freshify/api/security/JwtTokenFilter.java
@@ -1,17 +1,20 @@
 package no.freshify.api.security;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.security.SignatureException;
 import java.util.Arrays;
 
 /**
@@ -47,18 +50,24 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         }
 
         Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            Arrays.stream(cookies)
-                    .filter(cookie -> cookie.getName().equals("Authorization"))
-                    .findFirst().ifPresent(cookie -> {
-                        SecurityContextHolder.getContext()
-                                .setAuthentication(authenticationService.getAuthentication(cookie.getValue()));
-                        response.addCookie(
-                                CookieFactory.getAuthorizationCookie(
-                                        authenticationService.renewToken(cookie.getValue())
-                                )
-                        );
-                    });
+        try {
+            if (cookies != null && cookies.length != 0) {
+                Arrays.stream(cookies)
+                        .filter(cookie -> cookie.getName().equals("Authorization"))
+                        .findFirst().ifPresent(cookie -> {
+                            SecurityContextHolder.getContext()
+                                    .setAuthentication(authenticationService.getAuthentication(cookie.getValue()));
+                            response.addCookie(
+                                    CookieFactory.getAuthorizationCookie(
+                                            authenticationService.renewToken(cookie.getValue())
+                                    )
+                            );
+                        });
+            }
+        } catch (ExpiredJwtException e) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.getWriter().write("Invalid token");
+            return;
         }
 
         filterChain.doFilter(request, response);
