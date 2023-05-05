8cdbb3196f1ed5c3c0eaf0f8664dcd9ebb5c0b0b
Navid <Navidm@stud.ntnu.no>
2023-05-05 23:23:47 +0200

    fix: updated start application bat to close docker when pre existing

--- api/start_application.bat
+++ api/start_application.bat
@@ -1,3 +1,5 @@
+@echo off
+docker stop freshify_dev_database >nul 2>&1
 docker run --rm -p 56000:7000 --name freshify_dev_database -e MYSQL_ROOT_PASSWORD=freshify -e MYSQL_DATABASE=freshify_dev -d mysql --port 7000 --character-set-server=utf8 --collation-server=utf8_unicode_ci
 echo Waiting for database to start...
 timeout /t 10
@@ -12,3 +14,7 @@ if errorlevel 1 (
 echo Application is now running in the background
 docker exec -i freshify_dev_database mysql -uroot -pfreshify --default-character-set=utf8 < ./src/main/resources/insert_item_types.sql
 docker exec -i freshify_dev_database mysql -uroot -pfreshify --default-character-set=utf8 < ./src/main/resources/insert_recipes.sql
+docker exec -i freshify_dev_database mysql -uroot -pfreshify --default-character-set=utf8 < ./src/main/resources/insert_users.sql
+docker exec -i freshify_dev_database mysql -uroot -pfreshify --default-character-set=utf8 < ./src/main/resources/insert_households.sql
+docker exec -i freshify_dev_database mysql -uroot -pfreshify --default-character-set=utf8 < ./src/main/resources/insert_shopping_lists.sql
+docker exec -i freshify_dev_database mysql -uroot -pfreshify --default-character-set=utf8 < ./src/main/resources/insert_items.sql
